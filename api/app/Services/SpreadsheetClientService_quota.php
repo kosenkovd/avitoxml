8e147c9b6f7bef3c3767632a894069f464b8ab2f
Depeche <lightzerling@gmail.com>
2021-03-05 01:27:15 +0300

    трайкатчи и логгинг

--- api/app/Services/SpreadsheetClientService_quota.php
+++ api/app/Services/SpreadsheetClientService_quota.php
@@ -1,9 +1,9 @@
 <?php
-    
-    
+
+
     namespace App\Services;
-    
-    
+
+
     use App\Configuration\Config;
     use App\Services\Interfaces\ISpreadsheetClientService;
     use DateTime;
@@ -15,13 +15,13 @@
     use Google_Service_Sheets;
     use Google_Service_Sheets_ValueRange;
     use Illuminate\Support\Facades\Log;
-    
-    class SpreadsheetClientService implements ISpreadsheetClientService {
+
+    class SpreadsheetClientService_quota implements ISpreadsheetClientService {
         private Config $config;
         private Google_Client $client;
         private Google_Service_Drive_Permission $drivePermissions;
         private Google_Service_Sheets $sheetsService;
-        
+
         /**
          * Creates new GoogleSheet from template.
          *
@@ -37,7 +37,7 @@
             $this->setPermissions($tableId);
             return $tableId;
         }
-        
+
         /**
          * Sets default permissions to Google object.
          *
@@ -48,21 +48,21 @@
             $this->client->addScope(Google_Service_Drive::DRIVE);
             $driveService = new Google_Service_Drive($this->client);
             $drivePermissions = new Google_Service_Drive_Permission();
-            
+
             $drivePermissions->setRole('writer');
             $drivePermissions->setType('anyone');
             $driveService->permissions->create($id, $drivePermissions);
-            
+
             $drivePermissions->setRole('writer');
             $drivePermissions->setType('user');
             $drivePermissions->setEmailAddress('wdenkosw@gmail.com');
             $driveService->permissions->create($id, $drivePermissions);
-            
+
             $drivePermissions->setRole('writer');
             $drivePermissions->setType('user');
             $drivePermissions->setEmailAddress('xml.avito@gmail.com');
             $driveService->permissions->create($id, $drivePermissions);
-            
+
             $drivePermissions->setRole('owner');
             $drivePermissions->setType('user');
             $drivePermissions->setEmailAddress('Ipagishev@gmail.com');
@@ -73,7 +73,7 @@
                     "transferOwnership" => true
                 ]);
         }
-        
+
         /**
          * GoogleServicesClient constructor.
          * @throws \Google\Exception
@@ -86,52 +86,55 @@
             $this->client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
             $this->client->setAccessType('offline');
             $this->client->setAuthConfig(__dir__. '/../Configuration/GoogleAccountConfig.json');
-            
+
             $this->sheetsService = new Google_Service_Sheets($this->client);
-            
+
             $this->drivePermissions = new Google_Service_Drive_Permission();
             $this->drivePermissions->setRole('writer');
             $this->drivePermissions->setType('anyone');
         }
-        
+
         /**
          * @inheritDoc
          * @throws Exception
          */
-        public function getFileModifiedTime(string $fileId) : ?DateTime
+        public function getFileModifiedTime(string $fileId, string $quotaUser) : ?DateTime
         {
             $this->client->addScope(Google_Service_Drive::DRIVE);
             $driveService = new Google_Service_Drive($this->client);
-            
+
             try
             {
                 $file = $driveService->files->get($fileId, [
-                    'fields' => 'modifiedTime, createdTime'
+                    'fields' => 'modifiedTime, createdTime',
+                    'quotaUser' => $quotaUser
                 ]);
             }
             catch (Exception $exception)
             {
                 throw $exception;
             }
-            
+
             if(is_null($file->getModifiedTime()))
             {
                 return DateTime::createFromFormat(DateTime::RFC3339_EXTENDED, $file->getCreatedTime());
             }
-            
+
             return DateTime::createFromFormat(DateTime::RFC3339_EXTENDED, $file->getModifiedTime());
         }
-        
+
         /**
          * @inheritDoc
          * @throws Exception
          */
-        public function getSpreadsheetCellsRange(string $spreadsheetId, string $range, bool $toRetry = true) : array
+        public function getSpreadsheetCellsRange(string $spreadsheetId, string $range, string $quotaUser, bool $toRetry = true) : array
         {
+            $optParams = [ 'quotaUser' => $quotaUser ];
+
             $service = new Google_Service_Sheets($this->client);
             try
             {
-                $values = $service->spreadsheets_values->get($spreadsheetId, $range)->getValues();
+                $values = $service->spreadsheets_values->get($spreadsheetId, $range, $optParams)->getValues();
             }
             catch (Exception $exception)
             {
@@ -140,18 +143,18 @@
                 {
                     throw $exception;
                 }
-                
+
                 sleep(60);
-                $values = $service->spreadsheets_values->get($spreadsheetId, $range)->getValues();
+                $values = $service->spreadsheets_values->get($spreadsheetId, $range, $optParams)->getValues();
             }
-            
+
             if(is_null($values))
             {
                 return [];
             }
             return $values;
         }
-        
+
         /**
          * @inheritDoc
          * @throws Exception
@@ -161,15 +164,18 @@
             string $range,
             array $values,
             array $params,
+            string $quotaUser,
             bool $toRetry = true) : void
         {
+            $params['quotaUser'] = $quotaUser;
+
             $body = new Google_Service_Sheets_ValueRange(
                 [
                     'values' => $values
                 ]
             );
             $service = new Google_Service_Sheets($this->client);
-            
+
             try
             {
                 $service->spreadsheets_values->update(
@@ -185,7 +191,7 @@
                 {
                     throw $exception;
                 }
-                
+
                 sleep(60);
                 $service->spreadsheets_values->update(
                     $spreadsheetId,
@@ -195,16 +201,21 @@
                 );
             }
         }
-        
+
         /**
          * @inheritDoc
          * @throws Exception
          */
         public function updateCellContent(
-            string $tableID, string $targetSheet, string $cell, string $content, bool $toRetry = true): void
+            string $tableID,
+            string $targetSheet,
+            string $cell,
+            string $content,
+            string $quotaUser,
+            bool $toRetry = true): void
         {
             $range = $targetSheet.'!' . $cell . ':' . $cell;
-            
+
             $values = [
                 [$content]
             ];
@@ -216,23 +227,26 @@
                 $range,
                 $values,
                 $params,
+                $quotaUser,
                 $toRetry
             );
         }
-        
+
         /**
          * @inheritDoc
          */
-        public function getSheets(string $tableId): array
+        public function getSheets(string $tableId, string $quotaUser): array
         {
+            $optParams = [ 'quotaUser' => $quotaUser ];
+
             $sheets = [];
             $service = new Google_Service_Sheets($this->client);
-            
-            $response = $service->spreadsheets->get($tableId);
+
+            $response = $service->spreadsheets->get($tableId, $optParams);
             foreach($response->getSheets() as $s) {
                 $sheets[] = $s['properties']['title'];
             }
-            
+
             return $sheets;
         }
     }
