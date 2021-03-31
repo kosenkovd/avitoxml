a59f1dd32aba4dd6e280d00cf26c712dd0603854
Дмитрий Подольский <podolsky@techart.ru>
2021-03-31 17:19:26 +0300

    лимит maxAds

--- api/app/Services/Interfaces/IXmlGenerationService.php
+++ api/app/Services/Interfaces/IXmlGenerationService.php
@@ -1,38 +1,39 @@
 <?php
 
-
 namespace App\Services\Interfaces;
-
 
 interface IXmlGenerationService
 {
-    /**
-     * Generates Avito XML for specified spreadsheet.
-     *
-     * @param string $spreadsheetId spreadsheet id.
-     * @param string $targetSheet sheet name to generate XML from.
-     * @throws \Exception
-     * @return string generated XML.
-     */
-    public function generateAvitoXML(string $spreadsheetId, string $targetSheet) : string;
-    
-    /**
-     * Generates Yandex XML for specified spreadsheet.
-     *
-     * @param string $spreadsheetId spreadsheet id.
-     * @param string $targetSheet sheet name to generate XML from.
-     * @throws \Exception
-     * @return string generated XML.
-     */
-    public function generateYandexXML(string $spreadsheetId, string $targetSheet) : string;
-    
-    /**
-     * Generates Ula XML for specified spreadsheet.
-     *
-     * @param string $spreadsheetId spreadsheet id.
-     * @param string $targetSheet sheet name to generate XML from.
-     * @throws \Exception
-     * @return string generated XML.
-     */
-    public function generateUlaXML(string $spreadsheetId, string $targetSheet) : string;
+	/**
+	 * Generates Avito XML for specified spreadsheet.
+	 *
+	 * @param string $spreadsheetId spreadsheet id.
+	 * @param string $targetSheet   sheet name to generate XML from.
+	 * @param int    $adLimit       max ads
+	 * @return string generated XML.
+	 * @throws \Exception
+	 */
+	public function generateAvitoXML(string $spreadsheetId, string $targetSheet, int $adsLimit): string;
+
+	/**
+	 * Generates Yandex XML for specified spreadsheet.
+	 *
+	 * @param string $spreadsheetId spreadsheet id.
+	 * @param string $targetSheet   sheet name to generate XML from.
+	 * @param int    $adLimit
+	 * @return string generated XML.
+	 * @throws \Exception
+	 */
+	public function generateYandexXML(string $spreadsheetId, string $targetSheet, int $adsLimit): string;
+
+	/**
+	 * Generates Ula XML for specified spreadsheet.
+	 *
+	 * @param string $spreadsheetId spreadsheet id.
+	 * @param string $targetSheet   sheet name to generate XML from.
+	 * @param int    $adLimit
+	 * @return string generated XML.
+	 * @throws \Exception
+	 */
+	public function generateUlaXML(string $spreadsheetId, string $targetSheet, int $adsLimit): string;
 }
